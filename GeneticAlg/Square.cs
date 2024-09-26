646d689100f9ffae0d95d8ee2337b5fc283aaec1
Го Хаоцзе <103988645+GuoHaojiee@users.noreply.github.com>
2024-09-27 00:59:14 +0300

    Add files via upload

---
+++ GeneticAlg/Square.cs
@@ -0,0 +1,22 @@
+﻿namespace GeneticAlgLib
+{
+    public class Square
+    {
+        public int SideLength { get; }
+        public int X { get; set; }  // Левый нижний угол
+        public int Y { get; set; }
+
+        public Square(int sideLength, int x = 0, int y = 0)
+        {
+            SideLength = sideLength;
+            X = x;
+            Y = y;
+        }
+
+        public Boolean IsOverlaps(Square other)
+        {
+            return X < other.X + other.SideLength && X + SideLength > other.X && //По оси X они перекрываются
+                   Y < other.Y + other.SideLength && Y + SideLength > other.Y; // По оси Y они перекрываются
+        }
+    }
+}
